3cffd215426cbfaaa36e6af025526a91ba16e056
Andrew Sprague <andrewsprague@outlook.com>
2018-08-17 18:09:58 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,9 +2,6 @@ def my_select(collection)
  # your code here!
  collection.select do |collect|
    if collect == true
-     return true
-   else
-     return false
-   end
+     return collect
  end
 end
